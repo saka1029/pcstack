a3bc4d902131cbcfe646f444f81a5afab5a14e22
saka1029 <saka1029@gmail.com>
2023-11-13 15:18:36 +0900

    Symbol追加。

--- src/main/java/pcstack/Context.java
+++ src/main/java/pcstack/Context.java
@@ -1,20 +1,23 @@
 package pcstack;
 
 import java.util.Deque;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.LinkedList;
+import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class Context {
 
-    public final Verb[] stack;
-    public final Deque<Iterator<Verb>> rstack;
+    final Verb[] stack;
     public int sp = 0;
+
+    final Deque<Iterator<Verb>> rstack = new LinkedList<>();
+    final Map<Symbol, Verb> globals = new HashMap<>();
     
     Context(int stackSize) {
         this.stack = new Verb[stackSize];
-        this.rstack = new LinkedList<>();
     }
     
     public static Context of(int stackSize) {
---
+++ src/main/java/pcstack/Symbol.java
@@ -0,0 +1,31 @@
+package pcstack;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+public class Symbol implements Verb {
+	
+    static final Map<String, Symbol> symbols = new HashMap<>();
+	public final String name;
+	
+	private Symbol(String name) {
+		this.name = name;
+	}
+	
+	public static Symbol of(String name) {
+	    return symbols.computeIfAbsent(name, n -> new Symbol(n));
+	}
+	
+	@Override
+	public void execute(Context c) {
+	    Verb value = c.globals.get(this);
+	    Objects.requireNonNull(value, "symbol '%s' undefined".formatted(this));
+		c.execute(value);
+	}
+
+	@Override
+	public String toString() {
+		return name;
+	}
+}
