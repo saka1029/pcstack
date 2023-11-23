ee939bb5fc5be797255c0b5d281ff61ce0c85d5c
saka1029 <saka1029@gmail.com>
2023-11-23 20:46:43 +0900

    Update Terminator.java

--- src/main/java/saka1029/pcstack/Terminator.java
+++ src/main/java/saka1029/pcstack/Terminator.java
@@ -1,5 +1,13 @@
 package saka1029.pcstack;
 
+/**
+ * <table border='1' style='border-collapse: collapse'>
+ * <tr><th>Enum値</th><th>Verbとして使用</th><th>Context.execute()の戻り値</th></tr>
+ * <tr><td>END</td><td>x</td><td>o</td></tr>
+ * <tr><td>BREAK</td><td>o</td><td>x</td></tr>
+ * <tr><td>YIELD</td><td>o</td><td>o</td></tr>
+ * </table>
+ */
 public enum Terminator implements Value {
     END,
     BREAK,
