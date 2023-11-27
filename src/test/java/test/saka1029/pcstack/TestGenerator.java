c7c30213e9813dfb5d1a2f806303642179228d81
saka1029 <saka1029@gmail.com>
2023-11-27 09:44:49 +0900

    Update TestGenerator.java

--- src/test/java/test/saka1029/pcstack/TestGenerator.java
+++ src/test/java/test/saka1029/pcstack/TestGenerator.java
@@ -14,6 +14,7 @@ public class TestGenerator {
         Context c = Context.of();
         assertEquals(Int.of(6), c.eval("0 '(1 yield 2 yield 3 yield) generator '+ for"));
         assertEquals(Int.of(6), c.eval("0 '(3 iota '(yield) for) generator '+ for"));
+        assertEquals(Int.of(6), c.eval("0 '(3 iota 'yield for) generator '+ for"));
     }
     
     @Test
@@ -22,29 +23,4 @@ public class TestGenerator {
         c.run("'('(iota '(yield) for) cons generator) 'gen define");
         assertEquals(Int.of(6), c.eval("0 3 gen '+ for"));
     }
-    
-    /**
-     * generatorの問題ではなく、forの問題？
-     * 
-     *         add("for", c -> {
-     *             Verb closure = c.pop();
-     *             for (Verb e : (Collection)c.pop()) {
-     *                 c.push(e);
-     *                 c.execute(closure);
-     *             }
-     *             output(c);
-     *         });
-     * 
-     * c.execute(closure)を繰返すので、stackにyieldが溜まってしまう。
-     * forを実行した後のstackは
-     * [1, yield, 2, yield, 3, yield]
-     * となっている。
-     * c.execute(closure)をc.executeAsList(closure)に変更するとうまくいく。
-     */
-    @Test
-    public void testFor() {
-        Context c = Context.of();
-        assertEquals(Int.of(6), c.eval("0 '(3 iota 'yield for) generator '+ for"));
-    }
-
 }
