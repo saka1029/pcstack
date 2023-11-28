73a8c8d8fe6f931863d11393c5e9c4ea4696754d
saka1029 <saka1029@gmail.com>
2023-11-28 21:57:02 +0900

    Update TestCPS.java

--- src/test/java/test/saka1029/pcstack/TestCPS.java
+++ src/test/java/test/saka1029/pcstack/TestCPS.java
@@ -2,73 +2,47 @@ package test.saka1029.pcstack;
 
 import static org.junit.Assert.assertEquals;
 
-import java.util.Arrays;
-
 import org.junit.Test;
 
 public class TestCPS {
 
-    interface F {
-        Object[] apply(Object... args);
-    }
-
-    interface C {
-        F apply(F f);
-    }
-
-    static Object[] reverse(Object... args) {
-        int size = args.length;
-        Object[] result = new Object[size];
-        for (int i = 0, j = size - 1; i < size; ++i, --j)
-            result[i] = args[j];
-        return result;
-    }
-
-    static Object car(Object... args) {
-        return args[0];
-    }
-
-    static Object[] cdr(Object... args) {
-        return Arrays.copyOfRange(args, 1, args.length);
-    }
-
     /**
-     * (define (cps-prim f) (lambda args (let ((r (reverse args))) ((car r) (apply f
-     * (reverse (cdr r)))))))
+     * 継続渡しスタイル - Wikipedia
+     * https://ja.wikipedia.org/wiki/%E7%B6%99%E7%B6%9A%E6%B8%A1%E3%81%97%E3%82%B9%E3%82%BF%E3%82%A4%E3%83%AB#:~:text=%E7%B6%99%E7%B6%9A%E6%B8%A1%E3%81%97%E3%82%B9%E3%82%BF%E3%82%A4%E3%83%AB%20(CPS%3A%20Continuation,%E8%AB%96%E6%96%87%E3%81%AB%E3%81%8A%E3%81%84%E3%81%A6%E5%B0%8E%E5%85%A5%E3%81%95%E3%82%8C%E3%81%9F%E3%80%82
+     * 
+     * (define (factorial n)
+     *     (f-aux n 1))
+     * (define (f-aux n a)
+     *     (if (= n 0)
+     *         a
+     *         (f-aux (- n 1) (* n a)))) ; 末尾再帰
      */
-    static C csp_prim = (F f) -> (Object... args) -> {
-        Object[] r = reverse(args);
-        return ((F) r[0]).apply(f.apply(reverse(cdr(r))));
-    };
+    static class ContinuationPassingStyle {
+        static int factorial(int n) {
+            return f_aux(n, 1);
+        }
 
-    /**
-     * (define (factorial n) (f-aux n 1)) (define (f-aux n a) (if (= n 0) a (f-aux
-     * (- n 1) (* n a)))) ; 末尾再帰
-     */
-    static int factorial(int n) {
-        return f_aux(n, 1);
-    }
+        static int f_aux(int n, int a) {
+            return n == 0
+                ? a
+                : f_aux(n - 1, n * a);
+        }
 
-    static int f_aux(int n, int a) {
-        return n == 0
-            ? a
-            : f_aux(n - 1, n * a);
-    }
+        static int factorial_loop(int n) {
+            return f_loop(n, 1);
+        }
 
-    static int factorial_loop(int n) {
-        return f_loop(n, 1);
-    }
-
-    static int f_loop(int n, int a) {
-        while (n > 0)
-            a *= n--;
-        return a;
+        static int f_loop(int n, int a) {
+            while (n > 0)
+                a *= n--;
+            return a;
+        }
     }
 
     @Test
     public void testFactorial() {
-        assertEquals(24, factorial(4));
-        assertEquals(24, factorial_loop(4));
+        assertEquals(24, ContinuationPassingStyle.factorial(4));
+        assertEquals(24, ContinuationPassingStyle.factorial_loop(4));
     }
 
     /*
@@ -98,6 +72,9 @@ public class TestCPS {
         NormalStyle.execute();  // -> 10
     }
 
+    /**
+     * 引数の数ごとにインタフェースを定義する場合。
+     */
     static class CPS0 {
         interface Cont { void apply(int a); } 
         interface C1 { void apply(Cont c, int a); } 
@@ -122,6 +99,33 @@ public class TestCPS {
         CPS0.execute();   // -> 10
     }
 
+    /**
+     * 継続を最後の引数とする場合。
+     */
+    static class CPS0_1 {
+        interface Cont { void apply(int a); } 
+        interface C1 { void apply(int a, Cont c); } 
+        interface C2 { void apply(int a, int b, Cont c); }
+
+        static C1 inc_c = (a, c) -> c.apply(a + 1);
+        static C2 add_c = (a, b, c) -> c.apply(a + b);
+
+        static void execute() {
+            add_c.apply(1, 3, v1 ->
+                add_c.apply(v1, 5, v2 ->
+                    inc_c.apply(v2, v3 ->
+                        System.out.println(v3))));
+        }
+    }
+
+    @Test
+    public void testCSP0_1Execute() {
+        CPS0_1.execute();   // -> 10
+    }
+
+    /**
+     * 結果を戻り値として返す場合。
+     */
     static class CPS1 {
         interface Cont { void apply(int a); } 
         interface C1 { void apply(Cont c, int a); } 
@@ -148,6 +152,9 @@ public class TestCPS {
         assertEquals(10, CPS1.execute());
     }
 
+    /**
+     * 可変長引数のインタフェースを定義する場合。
+     */
     static class CPS2 {
         interface Cont { void apply(int a); } 
         interface F { void apply(Cont c, int... args); } 
